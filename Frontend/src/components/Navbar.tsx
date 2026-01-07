b3e6dc1f63865b322bb49a3a445e04396c4f8e2d
Sudip Das <sudipkgec23@gmail.com>
2026-01-07 17:19:48 +0530

    navbar css changed

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = ({
   return (
     <nav className="bg-white dark:bg-slate-900 shadow-sm sticky top-0 z-40 border-b border-transparent dark:border-slate-800 transition-colors">
       <div className="max-w-7xl mx-auto px-4 sm:px-3 lg:px-8">
-        <div className="flex justify-evenly gap-3 md:justify-between h-16">
+        <div className="flex justify-between h-16">
           
           {/* Navbar Logo */}
           <div className="flex items-center cursor-pointer" onClick={() => setView('home')}>
